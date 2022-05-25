2edd9399e570185eda5caf562f0e47fd6a8f804b
cocatrip <cocatrip@yahoo.com>
2022-05-25 15:17:06 +0700

    fix heroku deployment

--- app.py
+++ app.py
@@ -7,21 +7,40 @@ app = Flask(__name__)
 app_root = os.path.dirname(os.path.abspath(__file__))
 
 
-@app.route('/upload', methods=['POST', 'GET'])
+@app.route('/upload', methods=['POST'])
 def upload():
+    # return render_template("loading.html")
+
     target = os.path.join(app_root, 'data')
     if not os.path.isdir(target):
         os.makedirs(target)
 
-    if request.method == "POST":
-        email = request.form["email"]
-        password = request.form["password"]
-        file = request.files["file"]
-        filename = str(uuid.uuid4()) + ".csv"
-        destination = '/'.join([target, filename])
-        file.save(destination)
+    email = request.form["email"]
+    password = request.form["password"]
+    file = request.files["file"]
+    filename = str(uuid.uuid4()) + ".csv"
+    destination = '/'.join([target, filename])
+    file.save(destination)
+    return render_template("loading.html", email=email, password=password, destination=destination)
+
+
+@app.route("/run", methods=['POST'])
+def run():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        data = request.json
+        email = data["email"]
+        password = data["password"]
+        destination = data["destination"]
         fill_logbook(email, password, destination)
-        return render_template("done.html")
+        return "filled!"
+    else:
+        return 'Content-Type not supported!'
+
+
+@app.route("/done")
+def done():
+    return render_template("done.html")
 
 
 @app.route("/")
